7e06fb1359cd7ec932e16b0999ccef7f6dc2a42e
Julian Hall <jh314159@googlemail.com>
2016-07-23 20:21:25 +0100

    Add test for conversion failure, already passing

--- tests/Main.hs
+++ tests/Main.hs
@@ -13,5 +13,7 @@ tests = testGroup "Tests" [
          testCase "Can convert a string to ConstrainedDynamic and back" $
                   fromDynamic (toDyn "hello") @?= Just "hello",
          testCase "Can convert a bool to ConstrainedDynamic and back" $
-                  fromDynamic (toDyn True) @?= Just True
+                  fromDynamic (toDyn True) @?= Just True,
+         testCase "Cannot convert string to CD and back to a bool" $
+                  fromDynamic (toDyn "hello") @?= (Nothing::Maybe Bool)
         ]
