377751e0197348aa3ac56030ab79b1e1514a213d
Julian Hall <jh314159@googlemail.com>
2016-07-24 08:44:57 +0100

    Add ClassCastable typeclass Allows writing code that varies depending on availability of a specified constraint.

--- src/Data/ConstrainedDynamic.hs
+++ src/Data/ConstrainedDynamic.hs
@@ -1,14 +1,38 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs,ConstraintKinds,RankNTypes,FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies,MultiParamTypeClasses,UndecidableInstances #-}
 
 module Data.ConstrainedDynamic where
 
 import Data.Typeable
+import GHC.Exts (Constraint)
 
-data ConstrainedDynamic constraints where
-    ConsDyn :: Typeable a => a -> ConstrainedDynamic constraints
+data ConstrainedDynamic cs where
+    ConsDyn :: (Typeable a, cs a) => a -> ConstrainedDynamic cs
 
-toDyn :: Typeable a => a -> ConstrainedDynamic b
+toDyn :: (Typeable a, cs a) => a -> ConstrainedDynamic cs
 toDyn obj = ConsDyn obj
 
-fromDynamic :: Typeable a => ConstrainedDynamic b -> Maybe a
+fromDynamic :: (Typeable a, cs a) => ConstrainedDynamic cs -> Maybe a
 fromDynamic (ConsDyn obj) = cast obj
+
+applyClassFn :: ConstrainedDynamic cs -> (forall a.cs a => a -> b) -> b
+applyClassFn (ConsDyn obj) f = f obj
+
+data MaybeHasClass (cs :: * -> Constraint) where
+    JustHasClass     :: cs a => a -> MaybeHasClass cs
+    DoesNotHaveClass :: MaybeHasClass cs
+
+maybeApplyClassFn :: (forall a . cs a => a -> b) -> b -> MaybeHasClass cs -> b
+maybeApplyClassFn fn _ (JustHasClass a) = fn a
+maybeApplyClassFn _ def DoesNotHaveClass = def
+
+class ClassCastable (csFrom :: * -> Constraint)
+                    (csTo :: * -> Constraint)
+                    (container :: (* -> Constraint) -> *) where
+    classCast :: container csFrom -> MaybeHasClass csTo
+
+instance ClassCastable a a ConstrainedDynamic where
+    classCast (ConsDyn obj) = JustHasClass obj
+instance {-# OVERLAPPABLE #-} ClassCastable a b ConstrainedDynamic where
+    classCast (ConsDyn _) = DoesNotHaveClass
+
--- tests/Main.hs
+++ tests/Main.hs
@@ -4,16 +4,29 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import Data.ConstrainedDynamic
 import Data.Typeable
-    
+
 main :: IO ()
 main = defaultMain tests
+
+type CDShow = ConstrainedDynamic Show
+type CDEq = ConstrainedDynamic Eq
 
 tests :: TestTree
 tests = testGroup "Tests" [
          testCase "Can convert a string to ConstrainedDynamic and back" $
-                  fromDynamic (toDyn "hello") @?= Just "hello",
+                  fromDynamic (toDyn "hello" :: CDShow) @?= Just "hello",
          testCase "Can convert a bool to ConstrainedDynamic and back" $
-                  fromDynamic (toDyn True) @?= Just True,
+                  fromDynamic (toDyn True :: CDShow) @?= Just True,
          testCase "Cannot convert string to CD and back to a bool" $
-                  fromDynamic (toDyn "hello") @?= (Nothing::Maybe Bool)
+                  fromDynamic (toDyn "hello" :: CDShow) @?= (Nothing::Maybe Bool),
+         testCase "Apply a function from a typeclass to a CD value" $
+                  (toDyn True :: CDShow) `applyClassFn` show @?= "True",
+         testCase "Valid classCast returns JustHasClass" $
+                  maybeApplyClassFn (const "cast OK") "cast failed"
+                                    (classCast (toDyn "hello" :: CDShow)
+                                         :: MaybeHasClass Show) @?= "cast OK",
+         testCase "Invalid classCast returns DoesNotHaveClass" $
+                  maybeApplyClassFn (const "cast OK") "cast failed"
+                                    (classCast (toDyn "hello" ::CDShow)
+                                         :: MaybeHasClass Eq) @?= "cast failed"
         ]
